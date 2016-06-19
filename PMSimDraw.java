6b0227c6b7eb1f2c971dd88fbc4a4f0771cb6d2b
gesingle <gesingle@users.noreply.github.com>
2016-06-18 22:35:53 -0700

    Update PMSimDraw.java
    Change size of sim window and added reference marks.

--- PMSimDraw.java
+++ PMSimDraw.java
@@ -15,26 +15,62 @@ public class PMSimDraw extends JPanel {
 	
 	public PMSimDraw(){
 				
-		setPreferredSize(new Dimension(400, 400));
+		setPreferredSize(new Dimension(550, 550));
 		setBackground(Color.WHITE);
 	}
 	
 	public void paintComponent(Graphics g){
 		
 		super.paintComponent(g);
-		g.drawRect(25, 25, 350, 350);
-		rocketFlight(g);
+		g.drawRect(50, 50, 450, 450);
 		
+		int xcor = 50;
+		int ycor = 500;
+		int tickcounter = 0;
+		
+		for(int i = 0; i < 10; i++){
+			g.drawLine(xcor, 500, xcor, 505);
+			if(i == 0){
+				g.drawString("" + tickcounter + "", xcor, 525);
+			}
+			else if(i == 2){
+				g.drawString("" + tickcounter + "", xcor - 8, 525);
+			}
+			else{
+				g.drawString("" + tickcounter + "", xcor - 12, 525);
+			}
+			xcor += 50;
+			tickcounter += 50;
+		}
+		
+		tickcounter = 0;
+		
+		for(int i = 0; i < 10; i++){
+			g.drawLine(45, ycor, 50, ycor);
+			if(i == 0){
+				g.drawString("" + tickcounter + "", 25, ycor + 5);
+			}
+			else if(i == 1){
+				g.drawString("" + tickcounter + "", 20, ycor + 5);
+			}
+			else{
+				g.drawString("" + tickcounter + "", 15, ycor + 5);
+			}
+			ycor -= 50;
+			tickcounter += 50;
+		}
+		
+		rocketFlight(g);	
 	}
 	
 	public void rocketFlight(Graphics g){
-	
-		double x2 = 0;
-		double y2 = 0;
+		
+		double x;
+		double y;
 		
 		for(int i = 0; i <= time; i++) {
-			x = 25 + xvel * i;
-			y = 370 - yvel * i - 0.5 * (-9.8) * Math.pow(i, 2);
+			x = 50 + xvel * i;
+			y = 495 - yvel * i - 0.5 * (-9.8) * Math.pow(i, 2);
 			g.setColor(Color.blue);
 			g.fillOval((int)x, (int)y, 5, 5);
 		}				
@@ -42,28 +78,22 @@ public class PMSimDraw extends JPanel {
 	
 	public void setFlightParameters(int v, int a){
 		
-		x = 25;
-		y = 375;
 		angle = a;
 		velocity = v;
 		xvel = velocity * Math.cos(Math.toRadians(angle));
 		yvel = velocity * Math.sin(Math.toRadians(angle));
-		time = -yvel / (0.5 * -9.8);
-		
+		time = -yvel / (0.5 * -9.8);	
 	}
 	
 	public double getTime(){
-		
 		return time;
 	}
 	
 	public double getAltitude(){
-		
 		return (yvel * time) / 2;
 	}
 	
 	public double getDist(){
-		
 		return xvel * time;
 	}
 	
